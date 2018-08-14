a64c35c63bbb44c9a59f6080edc6112b5a826adc
Alex Nicholson <alex@newborne.co.uk>
2018-08-14 21:53:04 +0100

    Added skills again.

--- resources/javascript/components/pages/Home.vue
+++ resources/javascript/components/pages/Home.vue
@@ -40,25 +40,46 @@
             </section>
         </div>
 
-        <div class="wrapper" style="display: none;">
+        <div class="wrapper">
+            <section>
+                <h2>About Me</h2>
+
+                <p>My passion for developing started in secondary school when I began to experiment with Java to allow myself to change my username in game or update a range of textures.</p>
+
+                <p>I then began to create small websites for the games in order to display basic information and show game images. For example, one site consisted of a linked system which used PHP and MySQL to display in game stats onto the website.</p>
+
+                <p>After creating and experimenting with these websites I became more intrigued and captivated by developing therefore wanted to develop my skills further. Consequently, I began to recreate shots from <a href="https://dribbble.com/" target="_blank">Dribbble</a> and convert them into working pages. This allowed me to greatly improve my knowledge and become more familiar with new technologies such as jQuery and Sass.</p>
+
+                <p>Upon obtaining my first development job I had the opportunity to develop my skills and knowledge regarding languages. Consequently, I am now able to create more advanced websites and applications, which can be previewed <router-link to="/projects">here</router-link>. During my three years of employment at <a href="https://newborne.co.uk" target="_blank">Newborne</a> I have familiarised myself with Laravel to build APIs, middleware & websites. I have used VueJS to create single page applications & plug dynamic components onto existing websites and Java to create an Android application. Furthermore, I was responsible for setting up new websites on the Linux server and deploying scripts to them.</p>
+
+                <p>Moreover, I am continuously looking for ways in which I can advance my skills and knowledge to further improve my work.</p>
+            </section>
+        </div>
+
+        <div class="wrapper no-padding">
+            <section class="cta">
+                <a href="mailto:&#104;&#101;&#108;&#108;&#111;&#064;&#097;&#108;&#101;&#120;&#110;&#105;&#099;&#104;&#111;&#108;&#115;&#111;&#110;&#046;&#099;&#111;&#046;&#117;&#107;" target="_blank">Email me</a>
+            </section>
+        </div>
+
+        <div class="wrapper">
             <section>
                 <h2>Development</h2>
 
                 <div class="two-column">
                     <div class="column">
                         <h3>Skills</h3>
-
                         <ul>
-                            <skill percentage="90">HTML5</skill>
-                            <skill percentage="90">CSS3</skill>
-                            <skill percentage="60">JavaScript ES6</skill>
-                            <skill percentage="85">jQuery</skill>
-                            <skill percentage="80">PHP</skill>
-                            <skill percentage="80">MySQL</skill>
-                            <skill percentage="50">Java</skill>
-                            <skill percentage="80">API Integration</skill>
-                            <skill percentage="80">Laravel</skill>
-                            <skill percentage="80">VueJS</skill>
+                            <li>HTML5</li>
+                            <li>CSS3</li>
+                            <li>JavaScript ES6</li>
+                            <li>jQuery</li>
+                            <li>PHP</li>
+                            <li>MySQL</li>
+                            <li>Java</li>
+                            <li>API Integration</li>
+                            <li>Laravel</li>
+                            <li>VueJS</li>
                         </ul>
                     </div>
 
@@ -74,22 +95,6 @@
                         </ul>
                     </div>
                 </div>
-            </section>
-        </div>
-
-        <div class="wrapper">
-            <section>
-                <h2>About Me</h2>
-
-                <p>My passion for developing started in secondary school when I began to experiment with Java to allow myself to change my username in game or update a range of textures.</p>
-
-                <p>I then began to create small websites for the games in order to display basic information and show game images. For example, one site consisted of a linked system which used PHP and MySQL to display in game stats onto the website.</p>
-
-                <p>After creating and experimenting with these websites I became more intrigued and captivated by developing therefore wanted to develop my skills further. Consequently, I began to recreate shots from <a href="https://dribbble.com/" target="_blank">Dribbble</a> and convert them into working pages. This allowed me to greatly improve my knowledge and become more familiar with new technologies such as jQuery and Sass.</p>
-
-                <p>Upon obtaining my first development job I had the opportunity to develop my skills and knowledge regarding languages. Consequently, I am now able to create more advanced websites and applications, which can be previewed <router-link to="/projects">here</router-link>. During my three years of employment at <a href="https://newborne.co.uk" target="_blank">Newborne</a> I have familiarised myself with Laravel to build APIs, middleware & endpoints. I have used VueJS to create single page applications & plug dynamic components onto existing websites and Java to create an Android application. Furthermore, I was responsible for setting up new websites on the Linux server and deploying scripts to them.</p>
-
-                <p>Moreover, I am continuously looking for ways in which I advance my skills and knowledge to further improve my work.</p>
             </section>
         </div>
     </div>
