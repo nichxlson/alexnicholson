c5fb1ff29185daf72cb719743c0eff404cf8f886
Alex Nicholson <alex@newborne.co.uk>
2018-08-16 00:54:40 +0100

    Added project screens.

--- resources/javascript/components/pages/Project.vue
+++ resources/javascript/components/pages/Project.vue
@@ -7,18 +7,24 @@
             </section>
         </div>
 
-        <div class="wrapper no-padding" v-if="project.link">
-            <section class="cta">
-                <a :href="project.link" target="_blank">View Website</a>
+        <div class="wrapper no-padding" v-if="project.images && project.images.length">
+            <section class="gallery no-padding">
+                <a data-fancybox="gallery" :href="'/assets/img/projects/screens/' + project.img + '/' + image" v-for="image in project.images" :style="{ backgroundImage: 'url(/assets/img/projects/screens/' + project.img + '/' + image + ')'}"></a>
             </section>
         </div>
 
-        <div class="wrapper" v-if="project.technologies" :class="{ 'no-top-padding': !project.link }">
+        <div class="wrapper" v-if="project.technologies" :class="{ 'no-top-padding': (!project.images || project.images.length == 0) }">
             <section class="technologies">
                 <h2>Technologies</h2>
                 <div class="technology" v-for="technology in project.technologies">
                     <span>{{ technology }}</span>
                 </div>
+            </section>
+        </div>
+
+        <div class="wrapper no-padding" v-if="project.link">
+            <section class="cta">
+                <a :href="project.link" target="_blank">View Website</a>
             </section>
         </div>
     </div>
--- resources/javascript/stores/ProjectStore.js
+++ resources/javascript/stores/ProjectStore.js
@@ -9,7 +9,8 @@ export default {
             img: 'wh',
             intro: 'Created an Android app that allows clients to view insurance policies, emergency contacts & closest office branches. Built a custom CMS & API in Laravel that account executives could use to manage customers.',
             link: null,
-            technologies: ['Java', 'HTML5', 'CSS3', 'PHP', 'Laravel', 'VueJS']
+            technologies: ['Java', 'HTML5', 'CSS3', 'JavaScript', 'VueJS', 'PHP', 'Laravel'],
+            images: ['screen-02.jpg', 'screen-01.jpg', 'screen-03.jpg', 'screen-09.jpg', 'screen-04.jpg', 'screen-05.jpg', 'screen-07.jpg', 'screen-08.jpg']
         }, {
             name: 'Zawaya Property Website',
             slug: 'zawaya-property-website',
@@ -17,7 +18,8 @@ export default {
             img: 'zawaya',
             intro: 'Built a responsive website in Craft CMS. Used VueJS throughout the website to create dynamic content such as property listings or popup contact boxes. Integrated the Mailchimp API into the CMS to create custom email templates based on selected properties. Utilised Laravel Mix to minify images and compress JavaScript and CSS files.',
             link: 'https://zawayaproperty.com/',
-            technologies: ['HTML5', 'CSS3', 'SCSS', 'JavaScript', 'VueJS', 'PHP', 'CraftCMS']
+            technologies: ['HTML5', 'CSS3', 'SCSS', 'JavaScript', 'VueJS', 'PHP', 'CraftCMS'],
+            images: ['screen-03.jpg', 'screen-04.jpg', 'screen-05.jpg', 'screen-06.jpg', 'screen-10.jpg', 'screen-12.jpg']
         }, {
             name: 'Coffee Bike London Website',
             slug: 'cbl-website',
@@ -25,7 +27,8 @@ export default {
             img: 'cbl',
             intro: 'Created a small responsive website in WordPress.',
             link: 'http://coffeebike.london/',
-            technologies: ['HTML5', 'CSS3', 'SCSS', 'JavaScript', 'jQuery', 'PHP', 'WordPress']
+            technologies: ['HTML5', 'CSS3', 'SCSS', 'JavaScript', 'jQuery', 'PHP', 'WordPress'],
+            images: ['screen-02.jpg', 'screen-03.jpg', 'screen-04.jpg', 'screen-05.jpg', 'screen-06.jpg', 'screen-08.jpg']
         }, {
             name: 'Functional Physiotherapy Website',
             slug: 'functional-physio-website',
@@ -33,7 +36,8 @@ export default {
             img: 'fp',
             intro: 'Built a responsive website to help a client improve online presence. The website was built on CraftCMS so all content is editable. VueJS used for a contact form that can be submitted without refreshing the page.',
             link: 'https://functional-physio.co.uk/',
-            technologies: ['HTML5', 'CSS3', 'SCSS', 'JavaScript', 'VueJS', 'PHP', 'CraftCMS']
+            technologies: ['HTML5', 'CSS3', 'SCSS', 'JavaScript', 'VueJS', 'PHP', 'CraftCMS'],
+            images: ['screen-01.jpg', 'screen-03.jpg', 'screen-06.jpg', 'screen-07.jpg']
         }, {
             name: 'Hywell Waters Website',
             slug: 'hywell-waters-website',
@@ -41,7 +45,8 @@ export default {
             img: 'hywell',
             intro: 'A website created for a photographer to help improve web presence. Built upon WordPress CMS so the photographer can add and update the different galleries on the website. Images are compressed and lazy loading is used to minimise load times across the website.',
             link: 'http://hywellwaters.com/',
-            technologies: ['HTML5', 'CSS3', 'SCSS', 'JavaScript', 'jQuery', 'PHP', 'WordPress']
+            technologies: ['HTML5', 'CSS3', 'SCSS', 'JavaScript', 'jQuery', 'PHP', 'WordPress'],
+            images: ['screen-01.jpg', 'screen-04.jpg', 'screen-02.jpg', 'screen-03.jpg']
         }, {
             name: 'WorldCOB Website',
             slug: 'worldcob-website',
@@ -49,7 +54,8 @@ export default {
             img: 'worldcob',
             intro: 'Built a responsive WordPress website for a client to upgrade the previous outdated website. Loading times and overall functionality has improved. Used Laravel to create a member zone that allowed users to view a directory of business and export the list.',
             link: 'http://www.worldcob.org/en/',
-            technologies: ['HTML5', 'CSS3', 'SCSS', 'JavaScript', 'jQuery', 'PHP', 'WordPress', 'Laravel']
+            technologies: ['HTML5', 'CSS3', 'SCSS', 'JavaScript', 'jQuery', 'PHP', 'WordPress', 'Laravel'],
+            images: ['screen-01.jpg', 'screen-02.jpg', 'screen-05.jpg', 'screen-10.jpg', 'screen-11.jpg']
         }, {
             name: 'Sohar Calculator App',
             slug: 'sohar-calculator-app',
@@ -57,7 +63,8 @@ export default {
             img: 'sohar-calculator',
             intro: 'Produced a web app that worked offline on iOS and Android devices. AngularJS was used to make sure everything worked as single page and didn\'t require a page refresh. Used PHP and MySQL to save entries to a database.',
             link: 'http://soharportandfreezone.com/pos-new/#/',
-            technologies: ['HTML5', 'CSS3', 'PHP', 'MySQL', 'AngularJS']
+            technologies: ['HTML5', 'CSS3', 'PHP', 'MySQL', 'AngularJS'],
+            images: ['screen-01.jpg', 'screen-02.jpg', 'screen-03.jpg', 'screen-04.jpg', 'screen-05.jpg']
         }, {
             name: 'Sansaya Website',
             slug: 'sansaya-website',
@@ -65,7 +72,8 @@ export default {
             img: 'sansaya',
             intro: 'A WordPress website built for a client so they could display information on services they provide. Used a WordPress plugin to add extra fields to pages so that the client could attach videos to specific services.',
             link: 'http://sansayaclinic.com/',
-            technologies: ['HTML5', 'CSS3', 'SCSS', 'JavaScript', 'jQuery', 'PHP', 'WordPress']
+            technologies: ['HTML5', 'CSS3', 'SCSS', 'JavaScript', 'jQuery', 'PHP', 'WordPress'],
+            images: ['screen-01.jpg', 'screen-02.jpg', 'screen-03.jpg', 'screen-09.jpg', 'screen-10.jpg', 'screen-11.jpg']
         }]
     },
 
